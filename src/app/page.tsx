4807cd84a1aed0b91ed863925abb14def3c3986f
donnwilzy <ask4godswill@gmail.com>
2025-06-20 05:32:24 +0100

    still settng components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,42 @@
 import { Button } from "@/components/ui/button";
+import {Input} from "@/components/ui/input";
+import { Progress } from "@radix-ui/react-progress";
 
 export default function Home() {
   return (
-  <div>
-    <p color="red">
-      OUR PROJECT HAS BEGUN
-    </p>
-  </div>
+    <div className="flex flex-col gap-4 items-center">
+      <div>
+      </div>
+      
+
+      
+
+      <div className="flex flex-col gap-4">
+        <Button variant="elevated">
+          Click me
+        </Button>
+        <Input
+          type="email"
+          placeholder="Type here..."
+          className="w-64 placeholder:tracking-wide shadow-lg"
+        />
+        <progress
+          className="w-64 h-4 bg-gray-200 rounded-lg shadow-lg"
+         
+        />
+        <textarea
+          placeholder="Type here..."
+          className="w-64 placeholder:tracking-wide shadow-lg"
+        />
+
+        <input
+          type="checkbox"
+          className="w-4 h-4 rounded shadow-lg"
+        />
+        <label className="ml-2">Check me</label>
+      </div>
+
+    </div>
   );
 }
  
